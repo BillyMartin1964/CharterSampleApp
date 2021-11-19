58929ab57f39e022c4b574233846034178e1cc04
BillyMartin1964 <billy@sitesculptors.com>
2021-11-19 17:30:47 -0500

    Add project files.

---
+++ CharterSampleApp/CharterSampleApp/AppShell.xaml.cs
@@ -0,0 +1,21 @@
+﻿using CharterSampleApp.ViewModels;
+using CharterSampleApp.Views;
+
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CharterSampleApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ CharterSampleApp/CharterSampleApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using CharterSampleApp.ViewModels;
+
+using System.ComponentModel;
+
+using Xamarin.Forms;
+
+namespace CharterSampleApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
