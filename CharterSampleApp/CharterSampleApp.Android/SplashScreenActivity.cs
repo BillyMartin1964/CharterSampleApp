e3b467ff715839ef6d173c60ffeed17a8dd89b14
BillyMartin1964 <billy@sitesculptors.com>
2021-11-19 19:33:28 -0500

    InitialAppPush

---
+++ CharterSampleApp/CharterSampleApp.Android/SplashScreenActivity.cs
@@ -0,0 +1,49 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Content.Res;
+using Android.OS;
+using Xamarin.Forms;
+
+namespace CharterSampleApp.Droid
+{
+    [Activity(Theme = "@style/Theme.SplashScreen", //Indicates the theme to use for this activity
+        MainLauncher = true, //Set it as boot activity
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+        NoHistory = true)]
+    //Doesn't place it in back stack
+    public class SplashScreenActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            if (Device.Idiom == TargetIdiom.Phone)
+                RequestedOrientation = ScreenOrientation.Portrait;
+
+            if (Device.Idiom == TargetIdiom.TV || Device.Idiom == TargetIdiom.Tablet)
+                RequestedOrientation = ScreenOrientation.SensorLandscape;
+
+
+            // Create your application here
+            //System.Threading.Thread.Sleep(2000); //Let's wait awhile...
+            //this.StartActivity(typeof(MainActivity));
+
+            var intent = new Intent(this, typeof(MainActivity));
+            if (Intent.Extras != null)
+                intent.PutExtras(Intent.Extras); // copy push info from splash to main
+            StartActivity(intent);
+        }
+
+        public override void OnConfigurationChanged(Configuration newConfig)
+        {
+            base.OnConfigurationChanged(newConfig);
+
+            if (Device.Idiom == TargetIdiom.Phone)
+                RequestedOrientation = ScreenOrientation.Portrait;
+
+            if (Device.Idiom == TargetIdiom.TV || Device.Idiom == TargetIdiom.Tablet)
+                RequestedOrientation = ScreenOrientation.SensorLandscape;
+        }
+    }
+}
