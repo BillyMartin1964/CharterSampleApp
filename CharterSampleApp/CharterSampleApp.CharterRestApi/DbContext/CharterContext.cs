b3d7c4fb5276570d43f2bae580a6b6b29c0a12f8
BillyMartin1964 <billy@sitesculptors.com>
2021-11-22 04:59:46 -0500

    Added DbContext

---
+++ CharterSampleApp/CharterSampleApp.CharterRestApi/DbContext/CharterContext.cs
@@ -0,0 +1,18 @@
+﻿using CharterSampleApp.CharterRestApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CharterSampleApp.CharterRestApi.DbContext
+{
+    public class CharterContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public CharterContext(DbContextOptions<CharterContext> options)
+            : base(options)
+        {
+                
+        }
+
+        public DbSet<UserAccount> CharterUserAccount { get; set; } = null!;
+        public DbSet<Address> UserAddress { get; set; } = null!;
+        public DbSet<BillingStatement> UserBillingStatement { get; set; } = null!;
+    }
+}
--- CharterSampleApp/CharterSampleApp.CharterRestApi/Program.cs
+++ CharterSampleApp/CharterSampleApp.CharterRestApi/Program.cs
@@ -1,15 +1,23 @@
+using CharterSampleApp.CharterRestApi.DbContext;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Web;
+using Microsoft.EntityFrameworkCore;
+using CharterSampleApp.CharterRestApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
+//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddDbContext<CharterContext>(opt =>
+    opt.UseInMemoryDatabase("CharterDb"));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -18,14 +26,19 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
 
 app.MapControllers();
 
