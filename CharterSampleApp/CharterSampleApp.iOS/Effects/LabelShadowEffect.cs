0c6ed1f3665e8fd0cf9cf249b48bec661b50b6ad
BillyMartin1964 <billy@sitesculptors.com>
2021-11-22 17:04:14 -0500

    Label Shadow Effect

--- CharterSampleApp/CharterSampleApp.iOS/Effects/LabelShadowEffect.cs
+++ CharterSampleApp/CharterSampleApp.iOS/Effects/LabelShadowEffect.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using CharterSampleApp.iOS.Effects;
 using CharterSampleApp.Effects;
+using CoreGraphics;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
@@ -17,9 +18,26 @@ namespace CharterSampleApp.iOS.Effects
 {
     public class LabelShadowEffect : PlatformEffect
     {
+       
         protected override void OnAttached()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var effect = (ShadowEffect)Element.Effects.FirstOrDefault(e => e is ShadowEffect);
+
+                if (effect != null)
+                {
+                    Container.Layer.CornerRadius = effect.Radius;
+                    Container.Layer.ShadowColor = effect.Color.ToCGColor();
+                    Container.Layer.ShadowOffset = new CGSize(effect.DistanceX, effect.DistanceY);
+                    Container.Layer.ShadowOpacity = effect.Opacity;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+            }
         }
 
         protected override void OnDetached()
