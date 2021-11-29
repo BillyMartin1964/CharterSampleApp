35a4bb0869472d9e1fcaa7b1980b1023c38b69d3
BillyMartin1964 <billy@sitesculptors.com>
2021-11-29 07:31:08 -0500

    Fixed small typo

--- CharterSampleApp/CharterSampleApp.CharterRestApi/Models/Keys.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CharterSampleApp.CharterRestApi.Models
-{
-    public class Keys
-    {
-        public const string CharterDbConnectionString = "Data Source=plesk6000.is.cc;Initial Catalog=CharterDb;Persist Security Info=True;User ID=charter001;Password=Charter!";
-    }
-}
